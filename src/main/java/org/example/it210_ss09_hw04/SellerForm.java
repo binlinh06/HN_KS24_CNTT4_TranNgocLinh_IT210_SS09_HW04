fc6a54b11dc3d44b6ebd0b25c08d45ac84defda2
binlinh06 <nglinh150606@gmail.com>
2026-04-21 07:34:44 +0700

    Update SellerForm.java

--- src/main/java/org/example/it210_ss09_hw04/SellerForm.java
+++ src/main/java/org/example/it210_ss09_hw04/SellerForm.java
@@ -1,4 +1,24 @@
 package org.example.it210_ss09_hw04;
-
 public class SellerForm {
+    // Bước 1
+    private String fullName;
+    private String email;
+
+    // Bước 2
+    private String shopName;
+    private String address;
+
+    // getter/setter
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getShopName() { return shopName; }
+    public void setShopName(String shopName) { this.shopName = shopName; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 }
+
